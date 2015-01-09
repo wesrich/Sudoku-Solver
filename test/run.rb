dd12cf9a1781f571f0bfe129c43b801379495f2a
Wes Rich <elopdat@gmail.com>
2015-01-08 21:50:18 -0500

    First solution, test...

--- test/run.rb
+++ test/run.rb
@@ -2,5 +2,16 @@ require 'minitest'
 require_relative '../solver.rb'
 
 describe Solver do
-  
+  def setup
+    @solver = Solver.new
+  end
+
+  it "solves the first puzzle" do
+    num = 1
+    puzzle = File.read('../puzzles/puzzle_#{num}.sudoku')
+    solution = @solver.solve(puzzle)
+    known_solution = File.read('../solutions/puzzle_#{num}.sudoku')
+
+    assert_equal known_solution, solution
+  end
 end
