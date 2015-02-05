2172872dafd813bb3a8bbe2020a2a8c432563eb2
Wes Rich <elopdat@gmail.com>
2015-02-05 16:30:03 -0500

    Get the next tests working.

--- test/board.rb
+++ test/board.rb
@@ -11,17 +11,15 @@ describe Board do
   end
 
   it "loads correct rows" do
-    skip "NYI"
-    assert_equal [8,0,0,5,0,4,0,0,7], @solver.send(:row, 1)
-    assert_equal [0,9,0,7,0,1,0,6,0], @solver.send(:row, 3)
-    assert_equal [2,0,7,8,1,3,6,0,4], @solver.send(:row, 6)
-    assert_equal [0,3,0,9,0,2,0,8,0], @solver.send(:row, 7)
+    assert_equal [8,0,0,5,0,4,0,0,7], @board.send(:row, 1)
+    assert_equal [0,9,0,7,0,1,0,6,0], @board.send(:row, 3)
+    assert_equal [2,0,7,8,1,3,6,0,4], @board.send(:row, 6)
+    assert_equal [0,3,0,9,0,2,0,8,0], @board.send(:row, 7)
   end
 
   it "loads correct columns" do
-    skip "NYI"
-    assert_equal [8,0,0,1,0,2,0,0,6], @solver.send(:col, 1)
-    assert_equal [5,0,7,0,0,8,9,0,3], @solver.send(:col, 4)
-    assert_equal [4,0,1,0,0,3,2,0,5], @solver.send(:col, 6)
+    assert_equal [8,0,0,1,0,2,0,0,6], @board.send(:col, 1)
+    assert_equal [5,0,7,0,0,8,9,0,3], @board.send(:col, 4)
+    assert_equal [4,0,1,0,0,3,2,0,5], @board.send(:col, 6)
   end
 end
