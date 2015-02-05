6e3356964e5c60a1a911d33684fa3b91543c5b29
Wes Rich <elopdat@gmail.com>
2015-02-05 18:22:05 -0500

    Clean up moved code.

--- lib/solver.rb
+++ lib/solver.rb
@@ -6,18 +6,11 @@ class Solver
 
   def initialize
     @board = Board.new
-    @loaded = false
   end
 
   def solve(puzzle = nil)
-    load_board(puzzle)
+    @board.load_board(puzzle)
     @board.find_possible_values
     @board
-  end
-
-  private
-
-  def load_board(puzzle)
-    @board.load_board(puzzle)
   end
 end
