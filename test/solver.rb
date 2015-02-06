c9bbc7793d234906af626e58890abb53183c4838
Wes Rich <elopdat@gmail.com>
2015-02-05 19:39:51 -0500

    Standardize trailing end-of-line on puzzles, and strip it in known, to standardize input

--- test/solver.rb
+++ test/solver.rb
@@ -8,7 +8,7 @@ describe Solver do
     file_name = Dir.entries('./puzzles/').
                     delete_if { |dir| ['.','..'].include?(dir) }.sample
     puzzle = File.read("./puzzles/#{file_name}")
-    known_solution = File.read("./solutions/#{file_name}")
+    known_solution = File.read("./solutions/#{file_name}").rstrip
 
     solution = @solver.solve(puzzle)
 
